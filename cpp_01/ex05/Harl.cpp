0c972c09d6caa07a740b2aaec0a76643a25c7895
Manar Bengharbi <manbengh@bess-f2r3s2.clusters.42paris.fr>
2025-05-06 20:21:55 +0200

    05 pas fini

---
+++ cpp_01/ex05/Harl.cpp
@@ -0,0 +1,36 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+    ;
+}
+
+Harl::~Harl()
+{
+    ;
+}
+
+void Harl::debug()
+{
+    std::cout << "DEBUG" << std::endl;
+}
+
+void Harl::info()
+{
+    std::cout << "INFO" << std::endl;
+}
+
+void Harl::warning()
+{
+    std::cout << "WARNING" << std::endl;
+}
+
+void Harl::error()
+{
+    std::cout << "ERROR" << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    
+}
---
+++ cpp_01/ex05/Harl.hpp
@@ -0,0 +1,25 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+class Harl
+{
+    private :
+
+        void debug();
+        void info();
+        void warning();
+        void error();
+    
+    public :
+
+        Harl();
+        ~Harl();
+        void complain(std::string level);
+
+};
+
+#endif
