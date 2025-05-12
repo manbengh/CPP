9e647f1e759f34f4cf21a90129f57ed8e00ae30d
Manar Bengharbi <manbengh@f2r2s20.clusters.42paris.fr>
2025-05-12 19:10:50 +0200

    ex05 DONE

--- cpp_01/ex05/Harl.cpp
+++ cpp_01/ex05/Harl.cpp
@@ -32,5 +32,16 @@ void Harl::error()
 
 void Harl::complain(std::string level)
 {
-    
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*functions[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    for (int i = 0; i < 4; i++)
+    {
+        if (level == levels[i])
+        {
+            for (int j = i; j < 4; j++)
+                (this->*functions[j])();
+            return;
+        }
+    }
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
