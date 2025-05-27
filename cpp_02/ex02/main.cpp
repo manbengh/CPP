b7b7f9c6c63585b504e4aaa548079185a3b7fd64
Manar Bengharbi <manbengh@f1r9s16.clusters.42paris.fr>
2025-05-27 18:26:32 +0200

    fini

--- cpp_02/ex02/main.cpp
+++ cpp_02/ex02/main.cpp
@@ -1,35 +1,35 @@
 #include "Fixed.hpp"
 
 
-// int main( void )
-// {
-//     Fixed a;
-//     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-//     std::cout << a << std::endl;
-//     std::cout << ++a << std::endl;
-//     std::cout << a << std::endl;
-//     std::cout << a++ << std::endl;
-//     std::cout << a << std::endl;
-//     std::cout << b << std::endl;
-//     std::cout << Fixed::max( a, b ) << std::endl;
-//     return 0;
-// }
-
-
-int main() {
+int main( void )
+{
     Fixed a;
-
-    std::cout << "a : " << a << std::endl;
-    std::cout << "++a : " << ++a << std::endl;
-    std::cout << "a++ : " << a++ << std::endl;
-    std::cout << "a après a++ : " << a << std::endl;
-
-    std::cout << "--a : " << --a << std::endl;
-    std::cout << "a-- : " << a-- << std::endl;
-    std::cout << "a après a-- : " << a << std::endl;
-
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
     return 0;
 }
+
+
+// int main() {
+//     Fixed a;
+
+//     std::cout << "a : " << a << std::endl;
+//     std::cout << "++a : " << ++a << std::endl;
+//     std::cout << "a++ : " << a++ << std::endl;
+//     std::cout << "a après a++ : " << a << std::endl;
+
+//     std::cout << "--a : " << --a << std::endl;
+//     std::cout << "a-- : " << a-- << std::endl;
+//     std::cout << "a après a-- : " << a << std::endl;
+
+//     return 0;
+// }
 
 
 // int main()
